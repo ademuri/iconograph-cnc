d7315d5a94412447487118d0051143875657ec8c
Adam Demuri <adam.demuri@gmail.com>
2020-12-23 18:39:59 -0700

    Import an SVG and draw its paths on screen

--- client/src/Client.java
+++ client/src/Client.java
@@ -1,11 +1,78 @@
+import java.io.IOException;
+
+import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
+import org.apache.batik.anim.dom.SVGOMPathElement;
+import org.apache.batik.bridge.BridgeContext;
+import org.apache.batik.bridge.DocumentLoader;
+import org.apache.batik.bridge.GVTBuilder;
+import org.apache.batik.bridge.UserAgent;
+import org.apache.batik.bridge.UserAgentAdapter;
+import org.apache.batik.util.XMLResourceDescriptor;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.svg.SVGPoint;
+
 import processing.core.PApplet;
 
 
 public class Client extends PApplet {
 	// Develop in Eclipse following https://happycoding.io/tutorials/java/processing-in-java
+	// Note: Batik uses a really old version of the W3C Document APIs: https://stackoverflow.com/questions/13676937/how-to-find-package-org-w3c-dom-svg
 	
 	public void settings() {
-		size(1280, 720);
+		size(1920, 1280);
+	}
+	
+	public void setup() {
+		final Document doc;
+		try {
+		    String parser = XMLResourceDescriptor.getXMLParserClassName();
+		    SAXSVGDocumentFactory f = new SAXSVGDocumentFactory(parser);
+		    doc = f.createDocument("example.svg");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			exit();
+			return;
+		}
+		
+		// See https://stackoverflow.com/questions/26027313/how-to-load-and-parse-svg-documents
+		UserAgent userAgent = new UserAgentAdapter();
+	    DocumentLoader loader = new DocumentLoader(userAgent);
+	    BridgeContext bridgeContext = new BridgeContext(userAgent, loader);
+	    bridgeContext.setDynamicState(BridgeContext.DYNAMIC);
+
+	    // Enable CSS- and SVG-specific enhancements.
+	    (new GVTBuilder()).build(bridgeContext, doc);
+		
+		Element rootElement = doc.getDocumentElement();
+		traverse(rootElement);
+	}
+	
+	private void traverse(Element element) {
+		if (element instanceof SVGOMPathElement) {
+			SVGOMPathElement pathElement = (SVGOMPathElement) element;
+			float length = pathElement.getTotalLength();
+			float step = length / 10;
+			float scale = 3;
+			for (float i = 0; i < length; i += step) {
+				float endLength = i + step;
+				if (endLength > length) {
+					endLength = length;
+				}
+				
+				SVGPoint start = pathElement.getPointAtLength(i);
+				SVGPoint end = pathElement.getPointAtLength(endLength);
+				line(start.getX() * scale, start.getY() * scale, end.getX() * scale, end.getY() * scale);
+			}
+		}
+		
+		for (int i = 0; i < element.getChildNodes().getLength(); i++) {
+			Node child = element.getChildNodes().item(i);
+			if (child instanceof Element) {
+				traverse((Element) child);
+			}
+		}
 	}
 	
 	public void draw() {
@@ -20,7 +87,7 @@ public class Client extends PApplet {
 
 
 	public static void main(String[] args) {
-		String[] processingArgs = {"MySketch"};
+		String[] processingArgs = {"Iconograph CNC Client"};
 		Client client = new Client();
 		PApplet.runSketch(processingArgs, client);
 	}
