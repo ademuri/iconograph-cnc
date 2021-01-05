d82eec68260ddcae0b6e9417a7fc965607407433
Adam Demuri <adam.demuri@gmail.com>
2021-01-04 17:12:07 -0700

    Use advanced kinematics.
    This implements the algorithm from this answer: https://robotics.stackexchange.com/a/11410

--- client/download_deps.sh
+++ client/download_deps.sh
@@ -9,4 +9,7 @@ pushd deps
 wget https://phoenixnap.dl.sourceforge.net/project/ini4j/ini4j-bin/0.5.4/ini4j-0.5.4-bin.zip
 unzip ini4j-0.5.4-bin.zip
 
+wget https://ftp.wayne.edu/apache//commons/math/binaries/commons-math3-3.6.1-bin.zip
+unzip commons-math3-3.6.1-bin.zip
+
 popd
